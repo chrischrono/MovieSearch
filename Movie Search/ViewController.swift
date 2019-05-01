850907d12c2ca3c2c4a4fc9611b131eee7b00ed2
Christian <chris_chrono@yahoo.com>
2019-05-01 12:16:57 +0700

    Initial Commit

---
+++ Movie Search/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Movie Search
+//
+//  Created by Christian on 01/05/19.
+//  Copyright © 2019 Christian. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
